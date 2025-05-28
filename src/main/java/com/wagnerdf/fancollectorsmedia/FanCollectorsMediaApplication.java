fa27b825b7e75757085971bca02227f2b87d1439
Wagner Andrade <Wagnerandrade_df@hotmail.com>
2025-05-28 10:26:00 -0300

    Projeto inicial com dependências e configuração do banco

---
+++ src/main/java/com/wagnerdf/fancollectorsmedia/FanCollectorsMediaApplication.java
@@ -0,0 +1,13 @@
+package com.wagnerdf.fancollectorsmedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FanCollectorsMediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FanCollectorsMediaApplication.class, args);
+	}
+
+}
