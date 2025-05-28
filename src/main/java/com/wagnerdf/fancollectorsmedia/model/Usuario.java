f8a85f8eff42c5c6d339b9124536619df7a5174d
Wagner Andrade <Wagnerandrade_df@hotmail.com>
2025-05-28 11:41:57 -0300

    feat: criação da entidade Usuario e preparação para autenticação JWT

---
+++ src/main/java/com/wagnerdf/fancollectorsmedia/model/Usuario.java
@@ -0,0 +1,32 @@
+package com.wagnerdf.fancollectorsmedia.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Usuario {
+	
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    @Column(unique = true)
+    private String email;
+
+    private String senha;
+
+    private String papel; // Ex: "ROLE_USER", "ROLE_ADMIN"
+}
