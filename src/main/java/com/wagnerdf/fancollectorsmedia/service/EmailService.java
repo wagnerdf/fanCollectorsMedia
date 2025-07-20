d7c1e2a21859f9333fa96b022e4a57ef37a8de56
Wagner Andrade <Wagnerandrade_df@hotmail.com>
2025-07-20 13:07:02 -0300

    feat: configurada URL do frontend nos e-mails de recuperação usando @Value e variaveis de local e dev.

--- src/main/java/com/wagnerdf/fancollectorsmedia/service/EmailService.java
+++ src/main/java/com/wagnerdf/fancollectorsmedia/service/EmailService.java
@@ -1,6 +1,7 @@
 package com.wagnerdf.fancollectorsmedia.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -10,10 +11,13 @@ public class EmailService {
 
     @Autowired
     private JavaMailSender mailSender;
+    
+    @Value("${app.frontend.url}")
+    private String frontendUrl;
 
     public void enviarEmailRecuperacao(String destinatario, String token) {
         String assunto = "Recuperação de Senha - fanCollectorsMedia";
-        String link = "http://localhost:3000/resetar-senha?token=" + token;
+        String link = frontendUrl + "/resetar-senha?token=" + token;
         String conteudo = "Olá!\n\nClique no link abaixo para redefinir sua senha:\n\n" + link +
                 "\n\nSe você não solicitou isso, ignore este e-mail.";
 
