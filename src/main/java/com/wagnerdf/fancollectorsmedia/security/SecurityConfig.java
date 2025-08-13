3ce337752488e536082a4699c41b62eab8ed0b39
Wagner Andrade <Wagnerandrade_df@hotmail.com>
2025-08-13 16:42:08 -0300

    Corrige CORS permitindo PATCH e garante comunicação com frontend React

--- src/main/java/com/wagnerdf/fancollectorsmedia/security/SecurityConfig.java
+++ src/main/java/com/wagnerdf/fancollectorsmedia/security/SecurityConfig.java
@@ -84,7 +84,7 @@ public class SecurityConfig {
         	    "http://localhost:3000",
         	    "https://fan-collectors-frontend-app.vercel.app/"
         	));
-        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
 
