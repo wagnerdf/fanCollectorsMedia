8d0bd5a818275cfe740c9d82b866e130c69f0a9d
Wagner Andrade <Wagnerandrade_df@hotmail.com>
2025-11-11 21:19:16 -0300

    chore: desativar execução automática de jobs agendados (MidiaResetScheduler)
    Comentado o @EnableScheduling na classe principal
    FanCollectorsMediaApplication
    para desativar a execução automática de tarefas agendadas, como o
    MidiaResetScheduler.
    Essa mudança impede que o reset periódico de mídias seja executado até
    nova ativação.

--- src/main/java/com/wagnerdf/fancollectorsmedia/FanCollectorsMediaApplication.java
+++ src/main/java/com/wagnerdf/fancollectorsmedia/FanCollectorsMediaApplication.java
@@ -2,10 +2,10 @@ package com.wagnerdf.fancollectorsmedia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
+//import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
+//@EnableScheduling  //Desativa os serviços programados por periodo, sendo um deles o da pasta JOBS que faz entrada de script no banco de dados
 public class FanCollectorsMediaApplication {
 
 	public static void main(String[] args) {
